25af72ad2c61eb52c9a68133df8018feaa7e2569
Letícia Marchini <87884387+leticiamarchini@users.noreply.github.com>
2022-03-17 21:07:14 -0300

    atividades dia 02-03

---
+++ Exercicio02/Exercicio02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe um valor em R$: ");
+            string valor = Console.ReadLine();
+            decimal valorConvertido = 0.0m;
+            decimal valorCambio = 0.1932m;
+
+            decimal.TryParse(valor, out valorConvertido);
+
+            decimal valorCambioConvertido = valorConvertido * valorCambio;
+
+            Console.WriteLine($"Valor convertido: {valorCambioConvertido}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercicio03/exercicio03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a distancia percorrida em km:");
+            decimal.TryParse(Console.ReadLine(), out decimal kmPercorrido);
+
+            Console.WriteLine("Informe o combustivel gasto na viagem em litros:");
+            decimal.TryParse(Console.ReadLine(), out decimal litrosGastos);
+
+            Console.WriteLine($"Sua média foi de {kmPercorrido / litrosGastos}");
+            Console.ReadLine();
+        }
+    }
+}
